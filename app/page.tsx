c83942fdb36e2b32f341383adfea4fca7aa338f5
eliziahsalcedo29 <241882703+eliziahsalcedo29@users.noreply.github.com>
2025-12-02 11:40:08 +0800

    Added : Routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
-      <h6><b>Hello BulSU</b></h6>
+      <h1><b>Hello BulSU</b></h1>
+       <div>
+        <Link href="/login">Login</Link>
     </div>
+    </div>
+    
   );
 }
